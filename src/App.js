c2c1fcf5b190be6ebc3f6e659dc3d8e1482d3609
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-12-27 12:29:22 +0700

    sua ten

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Login from "./page/Login";
-import Register from "./page/Register";
+import Login from "./page/login";
+import Register from "./page/register";
 import Navbar from "./components/NavBar";
 
 function App() {
