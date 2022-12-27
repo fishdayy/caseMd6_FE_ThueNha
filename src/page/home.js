59a27e8c6584d88297d64a22a8b8503ddf3ba311
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-12-27 13:24:52 +0700

    test

--- src/page/home.js
+++ src/page/home.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import {useSelector} from "react-redux";
-
-const Home = () => {
-    const userNow = useSelector(state=>{
-        return state.user
-    })
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-};
-
-export default Home;
+export default function Home(){
+    return(
+        <>
+        </>
+    )
+}
--- src/page/login.js
+++ src/page/login.js
@@ -1,53 +1,33 @@
-import React from 'react';
-import {Field, Form, Formik} from "formik";
-import {useDispatch} from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
-import {login} from "../service/userService";
+import "./CSS/login.css"
 
-const Login = () => {
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const handleLogin = async (values) => {
-        let dataLogin = await dispatch(login(values))
-        checkLogin(dataLogin)
-    }
+export default function Login(){
 
-    const checkLogin = (dataLogin) => {
-        if (dataLogin.payload.mess) {
-            alert('Tài khoản hoặc mật khẩu không đúng')
-            localStorage.clear()
-
-        } else {
-            alert('Đăng nhập thành công')
-            navigate('/home')
-        }
-    }
-
-    return (
-        <div className="veen">
-            <div className="wrapper">
-                <form id="login" tabIndex="500">
-                    <h3>Login</h3>
-                    <div className="mail">
-                        <input type="mail" name=""/>
-                        <label>Mail or Username</label>
-                    </div>
-                    <div className="passwd">
-                        <input type="password" name=""/>
-                        <label>Password</label>
-                    </div>
-                    <div className="submit">
-                        <button className="dark">Login</button>
-                    </div>
-                    <div className="submit">
-                        <p>Don't have an account?</p>
-                        <button className="dark">Register</button>
-                    </div>
-                </form>
+    return(
+        <>
+            <div className="veen">
+                <div className="wrapper">
+                    <form id="login" tabIndex="500">
+                        <h3>Login</h3>
+                        <div className="mail">
+                            <input type="mail" name=""/>
+                                <label>Mail or Username</label>
+                        </div>
+                        <div className="passwd">
+                            <input type="password" name=""/>
+                                <label>Password</label>
+                        </div>
+                        <div className="submit">
+                            <button className="dark">Login</button>
+                        </div>
+                        <div className="submit">
+                            <p>Don't have an account?</p>
+                            <button className="dark">Register</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
-};
 
-export default Login;
+        </>
+    )
+}
