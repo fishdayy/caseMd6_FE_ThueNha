800ccefe1c64835d3cd6cd240beb25d8d8edce0e
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-12-28 09:53:42 +0700

    demo search

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Login from "./page/login";
 import Register from "./page/register";
 import Navbar from "./components/NavBar";
+import Banner from "./components/Banner";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+        <Navbar/>
+        <Banner/>
+        <Search/>
         {/*<Login/>*/}
       {/*  <Register/>*/}
     </div>
---
+++ src/components/Search.js
@@ -0,0 +1,66 @@
+import '../page/CSS/login.css'
+
+export default function Search() {
+    return (
+        <div className="container">
+            <ul className="nav nav-pills mb-3  nav-pills-flex">
+                <li class="nav-item" role="presentation">
+                    <button className="nav-link pills-home-tab" id="pills-home-tab" data-bs-toggle="#pills-home"
+                            role="tab" aria-controls="pills-home">
+                        <div style={{display:"flex",alignItems:"center"}}>
+                            <i className="fa-solid fa-home" style={{color:"black",padding:"0 10px"}}></i>
+                            <div className="nav-link-strong">
+                                <strong style={{color:"black"}}>
+                                    Hotel
+                                </strong>
+                            </div>
+                        </div>
+                    </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link pills-home-tab" id="pills-home-tab" data-bs-toggle="#pills-home"
+                            role="tab" aria-controls="pills-home">
+                        <div style={{display: "flex", alignItems: "center"}}>
+                            <i className="fa-solid fa-dungeon" style={{color: "black", padding: "0 10px"}}></i>
+                            <div className="nav-link-strong">
+                                <strong style={{color: "black"}}>
+                                    Homestay
+                                </strong>
+                            </div>
+                        </div>
+                    </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link pills-home-tab" id="pills-home-tab" data-bs-toggle="#pills-home"
+                            role="tab" aria-controls="pills-home">
+                        <div style={{display: "flex", alignItems: "center"}}>
+                            <i className="fa-solid fa-hotel" style={{color: "black", padding: "0 10px"}}></i>
+                            <div className="nav-link-strong">
+                                <strong style={{color: "black"}}>
+                                    Hostel
+                                </strong>
+                            </div>
+                        </div>
+                    </button>
+                </li>
+            </ul>
+            <ul className="nav nav-pills mb-3  nav-pills-flex">
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <i className="fa-solid fa-magnifying-glass" style={{padding:"0 10px"}}></i>
+                    <input type="text" placeholder="Where are you going?" className="text-search"/>
+                </div>
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <i className="fa-solid fa-calendar-days" style={{padding:"0 10px"}}></i>
+                    <span className="text-search">date to date</span>
+                </div>
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <i className="fa-solid fa-people-group" style={{padding:"0 10px"}}></i>
+                    <span className="text-search">1 bathroom, 1 bedroom</span>
+                </div>
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <button className="btn btn-outline-success" type="submit" style={{marginLeft:"10px"}}>Search</button>
+                </div>
+            </ul>
+        </div>
+    )
+}
